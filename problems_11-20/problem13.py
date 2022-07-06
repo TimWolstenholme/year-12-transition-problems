cac47c9cd7c3a9025795522142792ddfc3a00386
Tim Wolstenholme <timwolstenholme06@gmail.com>
2022-07-06 23:26:26 +0100

    Completed problem 13 Used a horrible lambda - took 10 mins to do!

---
+++ problems_11-20/problem13.py
@@ -0,0 +1,11 @@
+#Caesar cipher
+apply_shift=lambda letter,shift: chr(ord(letter)+shift) if not  (ord(letter)+shift<91 and letter.upper()==letter) and (ord(letter)+shift<123 and letter.lower()==letter) else chr(ord(letter)+shift-26) if letter !=' ' else letter
+shift=int(input("What is the shift of the caesar cipher "))
+while shift>26 or shift<1:
+    print("Shift must be in the range 0<shift<27")
+    shift=int(input("What is the shift of the caesar cipher "))
+string=input("What is the string that you are applying the cipher to ")
+string=list(string)
+string=[apply_shift(letter,shift) for letter in string ]
+string=''.join(letter for letter in string)
+print(string)
