c14cb870ba71298388dfe186a56ff41b02d1bd67
Tim Wolstenholme <timwolstenholme06@gmail.com>
2022-07-09 16:44:36 +0100

    fixed error in the lambda

--- problems_11-20/problem13.py
+++ problems_11-20/problem13.py
@@ -1,5 +1,5 @@
 #Caesar cipher
-apply_shift=lambda letter,shift: chr(ord(letter)+shift) if not  (ord(letter)+shift<91 and letter.upper()==letter) and (ord(letter)+shift<123 and letter.lower()==letter) else chr(ord(letter)+shift-26) if letter !=' ' else letter
+apply_shift=lambda letter,shift: chr(ord(letter)+shift) if not  (ord(letter)+shift<91 and letter.upper()==letter) and (ord(letter)+shift<123 and letter.lower()==letter) else chr(ord(letter)+shift-26) if not letter.isalpha() else letter
 shift=int(input("What is the shift of the caesar cipher "))
 while shift>26 or shift<1:
     print("Shift must be in the range 0<shift<27")
