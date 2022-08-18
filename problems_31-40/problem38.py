2cf36f41ac5df883d813142212d6a8169c061891
Tim Wolstenholme <timwolstenholme06@gmail.com>
2022-08-18 23:26:39 +0100

    completed problem 38

---
+++ problems_31-40/problem38.py
@@ -0,0 +1,5 @@
+greenbottles=int(input("What is the starting number: "))
+while greenbottles>0:
+    print(f"There was {greenbottles} greenbottles sitting on the wall, {greenbottles} greenbottles sitting on the wall, and if one greenbottle would accidently fall")
+    greenbottles-=1
+print("There was no green bottles sitting on the wall")
